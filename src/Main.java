5b8e99c449bf9a17d7a5d2bc109a4bc6a90ddf15
David John <djj@wfu.edu>
2021-01-27 12:35:01 -0500

    Initial commit of lab template

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+// Naive shopping experience where the input (an index) comes
+// from the command line, the store (an arraylist of myItem) is
+// hardcoded, and basic error checking is done.
+//
+// Your name
+// Date
+
+
+
+import java.util.ArrayList;
+
+public class Main {
+
+    // create Store
+    private static ArrayList<myItem> Store;
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/myItem.java
@@ -0,0 +1,34 @@
+// simple class to hold an item an its price
+//
+// Your name
+// Date
+
+public class myItem {
+    private String itemName;
+    private double itemPrice;
+
+
+    // Override Object methods:  toString() and equals(Object)
+    //
+    // David John
+    // January 2021
+
+    @Override
+    public String toString(){
+        return itemName + "  " + itemPrice;
+    }
+
+    @Override
+    public boolean equals(Object other){
+
+        // if the two objects are the same object then they are equal
+        if (this == other) return true;
+
+        // if other is not a myItem object then they are not equal
+        if (!(other instanceof myItem)) return false;
+
+        myItem temp = (myItem)other;
+        return this.itemName.equalsIgnoreCase(temp.itemName);
+    }
+}
+
